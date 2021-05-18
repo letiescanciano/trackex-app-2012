bd47a453baa1142c9c21b3c2f9da6538235f74c0
letiescanciano <leticia.escanciano@gmail.com>
2021-05-18 16:59:10 +0200

    session 4 - week 16

---
+++ src/components/Drawer/index.jsx
@@ -0,0 +1,197 @@
+import React from "react";
+
+import Drawer from "@material-ui/core/Drawer";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import Button from "@material-ui/core/Button";
+import styled from "styled-components";
+
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
+import { Debug } from "../../aux/Debug";
+
+const FormWrapper = styled.div`
+  padding: 16px;
+  width: 380px;
+  height: 100vh;
+  overflow: scroll;
+  background-color: #252f3d;
+  color: white;
+`;
+
+const RadioOptionsWrapper = styled.div`
+  display: flex;
+  padding-top: 24px;
+`;
+
+const FieldsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const ActionsWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const categories = [
+  { value: "eating_out", label: "Eating out" },
+  { value: "clothes", label: "Clothes" },
+  { value: "electronics", label: "Electronics" },
+  { value: "groceries", label: "Groceries" },
+  { value: "other", label: "Other" },
+  { value: "salary", label: "Salary" },
+];
+
+const types = [
+  { value: "expense", label: "Expense" },
+  { value: "income", label: "Income" },
+];
+
+const TransactionDrawer = (props) => {
+  console.log("TransactionDrawer props", props);
+  const {
+    mode,
+    open,
+    onClose,
+    transaction,
+    addTransaction,
+    editTransaction,
+  } = props;
+  const emptyFormInitialValues = {
+    name: "",
+    date: "",
+    amount: "",
+    category: "eating_out",
+    type: "expense",
+  };
+  const transactionSchema = Yup.object().shape({
+    name: Yup.string().required("Required field"),
+    date: Yup.string().required("Required field"),
+    amount: Yup.number().required("Required field"),
+    category: Yup.string().required("Required field"),
+    type: Yup.string().required("Required field"),
+  });
+
+  return (
+    <Drawer anchor='right' open={open} onClose={onClose}>
+      <FormWrapper>
+        <h2>{mode === "add" ? "New" : "Edit"} transaction</h2>
+        <Formik
+          initialValues={mode === "add" ? emptyFormInitialValues : transaction}
+          validationSchema={transactionSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            console.log("Values onSubmit", JSON.stringify(values));
+            mode === "add" ? addTransaction(values) : editTransaction(values);
+            setSubmitting(false);
+            onClose();
+          }}
+        >
+          {({
+            values,
+            handleChange,
+            touched,
+            errors,
+            isValid,
+            isSubmitting,
+          }) => (
+            <>
+              <Form>
+                <FieldsWrapper>
+                  <TextField
+                    fullWidth
+                    id='name'
+                    name='name'
+                    label='Name'
+                    value={values.name}
+                    onChange={handleChange}
+                    error={touched.name && Boolean(errors.name)}
+                    helperText={touched.name && errors.name}
+                  />
+                  <TextField
+                    fullWidth
+                    id='date'
+                    name='date'
+                    label='Date'
+                    value={values.date}
+                    onChange={handleChange}
+                    error={touched.date && Boolean(errors.date)}
+                    helperText={touched.date && errors.date}
+                  />
+                  <TextField
+                    fullWidth
+                    id='amount'
+                    type='number'
+                    name='amount'
+                    label='Amount'
+                    value={values.amount}
+                    onChange={handleChange}
+                    error={touched.amount && Boolean(errors.amount)}
+                    helperText={touched.amount && errors.amount}
+                  />
+                </FieldsWrapper>
+                <RadioOptionsWrapper>
+                  <FormControl component='fieldset'>
+                    <FormLabel component='legend'>Category</FormLabel>
+                    <RadioGroup
+                      aria-label='category'
+                      name='category'
+                      value={values.category}
+                      onChange={handleChange}
+                    >
+                      {categories.map((category) => (
+                        <FormControlLabel
+                          value={category.value}
+                          control={<Radio />}
+                          label={category.label}
+                        />
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                  <FormControl component='fieldset'>
+                    <FormLabel component='legend'>Type</FormLabel>
+                    <RadioGroup
+                      aria-label='type'
+                      name='type'
+                      value={values.type}
+                      onChange={handleChange}
+                    >
+                      {types.map(({ value, label }) => (
+                        <FormControlLabel
+                          value={value}
+                          control={<Radio />}
+                          label={label}
+                        />
+                      ))}
+                    </RadioGroup>
+                  </FormControl>
+                </RadioOptionsWrapper>
+                <ActionsWrapper>
+                  <Button variant='outlined' color='primary' onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    type='submit'
+                    disabled={!isValid || isSubmitting}
+                  >
+                    Save
+                  </Button>
+                </ActionsWrapper>
+              </Form>
+              {/* <Debug /> */}
+            </>
+          )}
+        </Formik>
+      </FormWrapper>
+    </Drawer>
+  );
+};
+
+export { TransactionDrawer };
--- src/components/Transactions/List/index.jsx
+++ src/components/Transactions/List/index.jsx
@@ -1,24 +1,14 @@
 import React, { useState, useEffect } from "react";
 
 import styled from "styled-components";
-import * as Yup from "yup";
 
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import EditIcon from "@material-ui/icons/Edit";
 import Button from "@material-ui/core/Button";
-import Drawer from "@material-ui/core/Drawer";
-import TextField from "@material-ui/core/TextField";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
 
-import { Formik, Form } from "formik";
-
-import { Debug } from "../../../aux/Debug";
 import data from "./data.js";
 
+import { TransactionDrawer } from "../../Drawer";
 const Table = styled.table`
   width: 100%;
   text-align: left;
@@ -53,64 +43,28 @@ const AddButtonWrapper = styled.div`
   justify-content: flex-end;
 `;
 
-const FormWrapper = styled.div`
-  padding: 16px;
-  width: 380px;
-  height: 100vh;
-  overflow: scroll;
-  background-color: #252f3d;
-  color: white;
-`;
-
-const RadioOptionsWrapper = styled.div`
-  display: flex;
-  padding-top: 24px;
-`;
-
-const FieldsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
-const ActionsWrapper = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-`;
-
-const categories = [
-  { value: "eating_out", label: "Eating out" },
-  { value: "clothes", label: "Clothes" },
-  { value: "electronics", label: "Electronics" },
-  { value: "groceries", label: "Groceries" },
-  { value: "other", label: "Other" },
-];
-
-const types = [
-  { value: "expense", label: "Expense" },
-  { value: "income", label: "Income" },
-];
+const AVAILABLE_MODES = {
+  add: "add",
+  edit: "edit",
+  read: "read",
+};
 
 const TransactionsList = () => {
   const [transactions, setTransactions] = useState([]);
   const [openDrawer, setOpenDrawer] = useState(false);
+  const [mode, setMode] = useState("add");
+  const [selectedTransaction, setSelectedTransaction] = useState({});
 
   useEffect(() => {
-    setTransactions(data);
+    setTimeout(() => {
+      setTransactions(data);
+    }, 2000);
   }, []);
 
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  });
-
-  const transactionSchema = Yup.object().shape({
-    name: Yup.string().required("Required field"),
-    date: Yup.string().required("Required field"),
-    amount: Yup.number().required("Required field"),
-    category: Yup.string().required("Required field"),
-    type: Yup.string().required("Required field"),
   });
 
   const handleDelete = (id) => {
@@ -120,10 +74,41 @@ const TransactionsList = () => {
     setTransactions(_transactions);
   };
 
+  const handleEdit = (id) => {
+    //in here we'll edit the selected transaction
+    console.log("edit id", id);
+    // 1. set mode to Edit
+    setMode("edit");
+    // 2. I need to find the selected transaction in my transactions array
+    const foundTransaction = transactions.find((transaction) => {
+      return transaction.id === id;
+    });
+    // 3. I need to setSelectedTransaction to the one I found (save it in my state)
+    setSelectedTransaction(foundTransaction);
+    // 4. open the drawer and fill out the form with the transaction data
+    setOpenDrawer(true);
+  };
   const addTransactionToList = (data) => {
     console.log("Data", data);
     setTransactions([...transactions, { ...data }]);
   };
+
+  const editTransaction = (data) => {
+    // console.log("data", data);
+    // 1. Find the transaction index to edit in the array
+    const transactionIndex = transactions.findIndex(
+      (transaction) => transaction.id === data.id
+    );
+    // 2. Make a copy of our transactions state
+    const _transactions = [...transactions];
+    // 3. Replace the transaction that we need to edit
+    _transactions[transactionIndex] = data;
+
+    // console.log("_transactions", _transactions);
+    // 4. Update our transactions (state) array
+    setTransactions(_transactions);
+  };
+
   return (
     <Container>
       <AddButtonWrapper>
@@ -135,172 +120,57 @@ const TransactionsList = () => {
           + Add Transaction
         </Button>
       </AddButtonWrapper>
-      <Table>
-        <thead>
-          <tr>
-            <HeadCell>Date</HeadCell>
-            <HeadCell>Name</HeadCell>
-            <HeadCell>Category</HeadCell>
-            <HeadCell>Amount</HeadCell>
-            <HeadCell></HeadCell>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map(({ id, date, name, category, type, amount }) => {
-            return (
-              <tr key={id}>
-                <TableCell>{date}</TableCell>
-                <TableCell>{name}</TableCell>
-                <TableCell>{category}</TableCell>
-                <TableCell>
-                  <Amount type={type}>{formatter.format(amount)}</Amount>
-                </TableCell>
-                <TableCell>
-                  <EditIcon
-                    style={{ marginRight: "16px" }}
-                    onClick={(e) => {
-                      console.log("I should open edit mode");
-                      console.log(e, id);
-                    }}
-                  />
-                  <DeleteForeverIcon
-                    style={{ color: "#FF7661" }}
-                    onClick={() => handleDelete(id)}
-                  />
-                </TableCell>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
+      {transactions.length > 0 ? (
+        <Table>
+          <thead>
+            <tr>
+              <HeadCell>Date</HeadCell>
+              <HeadCell>Name</HeadCell>
+              <HeadCell>Category</HeadCell>
+              <HeadCell>Amount</HeadCell>
+              <HeadCell></HeadCell>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map(({ id, date, name, category, type, amount }) => {
+              return (
+                <tr key={id}>
+                  <TableCell>{date}</TableCell>
+                  <TableCell>{name}</TableCell>
+                  <TableCell>{category}</TableCell>
+                  <TableCell>
+                    <Amount type={type}>{formatter.format(amount)}</Amount>
+                  </TableCell>
+                  <TableCell>
+                    <EditIcon
+                      style={{ marginRight: "16px" }}
+                      onClick={() => {
+                        handleEdit(id);
+                      }}
+                    />
+                    <DeleteForeverIcon
+                      style={{ color: "#FF7661" }}
+                      onClick={() => handleDelete(id)}
+                    />
+                  </TableCell>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      ) : (
+        "Loading...."
+      )}
 
       {openDrawer && (
-        <Drawer
-          anchor='right'
+        <TransactionDrawer
+          mode={mode}
           open={openDrawer}
           onClose={() => setOpenDrawer(false)}
-        >
-          <FormWrapper>
-            <h2>New transaction</h2>
-            <Formik
-              initialValues={{
-                name: "",
-                amount: "",
-                date: "",
-                category: "eating_out",
-                type: "expense",
-              }}
-              validationSchema={transactionSchema}
-              onSubmit={(values, { setSubmitting }) => {
-                console.log("Values onSubmit", JSON.stringify(values));
-                addTransactionToList(values);
-                setSubmitting(false);
-                setOpenDrawer(false);
-              }}
-            >
-              {({
-                values,
-                handleChange,
-                touched,
-                errors,
-                isValid,
-                isSubmitting,
-              }) => (
-                <>
-                  <Form>
-                    <FieldsWrapper>
-                      <TextField
-                        fullWidth
-                        id='name'
-                        name='name'
-                        label='Name'
-                        value={values.name}
-                        onChange={handleChange}
-                        error={touched.name && Boolean(errors.name)}
-                        helperText={touched.name && errors.name}
-                      />
-                      <TextField
-                        fullWidth
-                        id='date'
-                        name='date'
-                        label='Date'
-                        value={values.date}
-                        onChange={handleChange}
-                        error={touched.date && Boolean(errors.date)}
-                        helperText={touched.date && errors.date}
-                      />
-                      <TextField
-                        fullWidth
-                        id='amount'
-                        type='number'
-                        name='amount'
-                        label='Amount'
-                        value={values.amount}
-                        onChange={handleChange}
-                        error={touched.amount && Boolean(errors.amount)}
-                        helperText={touched.amount && errors.amount}
-                      />
-                    </FieldsWrapper>
-                    <RadioOptionsWrapper>
-                      <FormControl component='fieldset'>
-                        <FormLabel component='legend'>Category</FormLabel>
-                        <RadioGroup
-                          aria-label='category'
-                          name='category'
-                          value={values.category}
-                          onChange={handleChange}
-                        >
-                          {categories.map((category) => (
-                            <FormControlLabel
-                              value={category.value}
-                              control={<Radio />}
-                              label={category.label}
-                            />
-                          ))}
-                        </RadioGroup>
-                      </FormControl>
-                      <FormControl component='fieldset'>
-                        <FormLabel component='legend'>Type</FormLabel>
-                        <RadioGroup
-                          aria-label='type'
-                          name='type'
-                          value={values.type}
-                          onChange={handleChange}
-                        >
-                          {types.map(({ value, label }) => (
-                            <FormControlLabel
-                              value={value}
-                              control={<Radio />}
-                              label={label}
-                            />
-                          ))}
-                        </RadioGroup>
-                      </FormControl>
-                    </RadioOptionsWrapper>
-                    <ActionsWrapper>
-                      <Button
-                        variant='outlined'
-                        color='primary'
-                        onClick={() => setOpenDrawer(false)}
-                      >
-                        Cancel
-                      </Button>
-                      <Button
-                        variant='contained'
-                        color='primary'
-                        type='submit'
-                        disabled={!isValid || isSubmitting}
-                      >
-                        Save
-                      </Button>
-                    </ActionsWrapper>
-                  </Form>
-                  {/* <Debug /> */}
-                </>
-              )}
-            </Formik>
-          </FormWrapper>
-        </Drawer>
+          transaction={selectedTransaction}
+          addTransaction={addTransactionToList}
+          editTransaction={editTransaction}
+        />
       )}
     </Container>
   );
